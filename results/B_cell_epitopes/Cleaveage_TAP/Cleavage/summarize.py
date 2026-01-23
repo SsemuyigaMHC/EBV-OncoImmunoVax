0fa3abf57ba8be0f8b21afc4484cc7e2a3d9f5e9
Ssemuyiga Charles <107952446+SsemuyigaMHC@users.noreply.github.com>
2026-01-24 01:25:06 +0300

    Delete results/B_cell_epitopes directory

--- results/B_cell_epitopes/Cleaveage_TAP/Cleavage/summarize.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Load the csv file to inspect the contents
-csv_file = '/mnt/data/result.csv'
-data = pd.read_csv(csv_file)
-
-# Display the first few rows of the data
-data.head()
-
